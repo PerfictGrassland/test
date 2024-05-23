756fcde2465a852aaeb743c5477f417fc106cb0e
Chen, James SJ <82010157@corphq.hk.pccw.com>
2024-05-23 18:02:09 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,286 @@
+const error = {
+  "E-8003": "Please enter HKIC No.",
+  "E-8004": "Incomplete HKIC No. Please enter again.",
+  "E-8005": "Invalid HKIC No. Please enter again.",
+  "E-8006": "Please select Certificate Media Type.",
+  "E-2137": "Taxpayer identification number entered is same as your spouse's.",
+  "E-2138": "The MyGovHK Username entered is incorrect.",
+  "E-2139": "Please enter MyGovHK Username.",
+  "E-2144": "Please enter Password",
+  "E-8001": "Please enter Certificate Location.",
+  "E-8002": "Please enter PIN.",
+  "E-8008": "Digital certificate file does not exist.",
+  "E-8009": "Invalid PIN. Please enter again.",
+  "E-8010": "Invalid PIN for 3 times. Transaction terminated.",
+  "E-8012":
+    "Certificate Type selected does not match with your digital certificate.",
+  "E-8013": "HKIC No. does not match with your digital certificate.",
+  "E-8015": "Digital certificate has expired.",
+  "E-8016": "Digital certificate has been revoked or suspended.",
+  "E-8017": "Digital certificate not accepted.",
+  "E-8020": "Please enter TIN.",
+  "E-8029":
+    'According to our records, you do not have an active eTAX account. Please click "Apply for eTAX Password" button to apply for your Access Code.',
+  "E-8030":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8034": "Invalid Password. Please enter your Password again.",
+  "E-8036": "Please enter Password.",
+  "E-8037": "TIN must be 8-16 alpha-numeric.",
+  "E-8041":
+    "According to our records, your eTAX account has already been cancelled. Please call our hotline at (852)183 2011 during office hour.",
+  "E-8042":
+    'According to our records, your eTAX account has already been revoked. Please click "Apply for eTAX Password" button to apply for your eTAX Password again.',
+  "E-8043":
+    'According to our records, your eTAX account has expired. Please click "Apply for eTAX Password" button to apply for your eTAX Password again.',
+  "E-8047": "Invalid Password for 3 times. Transaction terminated.",
+  "E-8052": "No record found.  Please ensure the inputted TIN is correct.",
+  "E-8053":
+    "Record not found. Please contact our hotline at (852)183 2011 during office hours.",
+  "E-8057": "The file size of the digital certificate is larger than 100KB.",
+  "E-8080":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8090":
+    "To use the service, you must register with the Inland Revenue Department and obtain E-Stamp Account number.",
+  "E-8091": "Access Code/Password cancelled.",
+  "E-8092":
+    "You have entered your Access Code as Password.  Please activate your E-Stamp account first.",
+  "E-8093": "Incorrect Password.",
+  "E-8094":
+    "Invalid Password.  Your Password has been revoked.  Please register for the service again with the Inland Revenue Department.",
+  "E-8095": "Incorrect Password.",
+  "E-8096":
+    "Invalid Password.  Your Password has been revoked.  Please register for the service again with the Inland Revenue Department.",
+  "E-8097":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8200":
+    "Signing failed. Please try again. If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8202":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8203":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8204":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8205":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8206":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8210":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8211":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8212":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8213":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8214":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8215":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8217":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8220":
+    "Sorry, the system is unable to process your request.  Please try again later.  If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8225":
+    "The TIN/PASSWORD entered does not correspond to the signing person, please call our hotline at 183 2011 during office hour.",
+  "E-8226":
+    'The TIN/PASSWORD entered does not correspond to the signing person, please go back to the "Personal Particulars" Screen and amend.',
+  "E-8227":
+    'The TIN/PASSWORD entered does not correspond to the signing person, please go back to the "Personal Particulars" Screen and amend.',
+  "E-8228":
+    "The TIN/PASSWORD entered does not correspond to the signing person, please call our hotline at 183 2011 during office hour.",
+  "E-8229": "Your MyGovHK Username Account is not linked up.",
+  "E-8230":
+    "No record found. Please ensure the inputted MyGovHK Username is correct.",
+  "E-8231": "The MyGovHK Username entered is incorrect.",
+  "E-8232":
+    "Signing failed. Please try again. If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8233": "Your MyGovHK Username Account is outdated.",
+  "E-8234": "Your MyGovHK Username Account cannot be found.",
+  "E-8235": "Your MyGovHK Username Account is not activated.",
+  "E-8236": "Your MyGovHK Username Account is suspended.",
+  "E-8237": "Your MyGovHK Username Account is locked.",
+  "E-8238": "Your MyGovHK Username Account is deleted.",
+  "E-8239": "Your MyGovHK Username Account is not linked up.",
+  "E-8240": "Invalid Password. Please enter your Password again.",
+  "E-8241":
+    "Signing failed. Please try again. If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8242":
+    "Signing failed. Please try again. If problem persists, please call our hotline at (852)183 2011 during office hour.",
+  "E-8243": "MyGovHK Username entered is same as your spouse's.",
+  "E-8244": "Please enter MyGovHK Username.",
+  "E-8245": "Please enter Password.",
+  "E-8249":
+    "Certificate for digital signing does not match with that for account login",
+  "E-8261": "No record found. Please ensure the inputted username is correct.",
+  "E-8262":
+    'According to our records, your BTP user account has already been revoked. Please click "Forgot Username and/or Password" to reset your password.',
+  "E-8263":
+    "According to our records, your BTP user account is currently suspended. Please call our hotline at (852)183 2011 during office hour",
+  "E-8264":
+    "According to our records, your BTP account has already been cancelled. Please call our hotline at (852)183 2011 during office hour.",
+  "E-8265":
+    "Invalid Password. Please enter your Password again. Password registered under BTP Account should compose of 10 - 16 alpha-numeric characters and is case sensitive.",
+};
+
+const errorCn = {
+  "E-8003": "必須輸入香港身分證號碼。",
+  "E-8004": "輸入的香港身分證號碼不完整，請再輸入。",
+  "E-8005": "輸入的香港身分證號碼格式不符，請再輸入。",
+  "E-8006": "請選擇證書媒體類別。",
+  "E-2137": "輸入的税務編號與配偶的相同",
+  "E-2138": "輸入的「我的政府一站通」帳戶名稱不正確。",
+  "E-2139": "請輸入「我的政府一站通」帳戶。",
+  "E-2144": "請輸入密碼。",
+  "E-8001": "請輸入證書位置。",
+  "E-8002": "請輸入密碼。",
+  "E-8008": "數碼證書檔案並不存在。",
+  "E-8009": "輸入的密碼不正確，請再輸入。",
+  "E-8010": "已三次輸入不正確的密碼 ，操作終止。",
+  "E-8012": "輸入的證書類別與數碼證書不相符。",
+  "E-8013": "輸入的香港身分證號碼與數碼證書不相符。",
+  "E-8015": "數碼證書已過期。",
+  "E-8016": "數碼證書已被撤銷或暫停。",
+  "E-8017": "不接受此類數碼證書。",
+  "E-8020": "請輸入税務編號。",
+  "E-8029":
+    "你並沒有可運用的税務易帳戶 ，請點選“申請税務易通行密碼”申請啟動密碼。",
+  "E-8030":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8034":
+    "輸入不正確，請重新輸入通行密碼。舊有的通行密碼由 6個數字組成；而在現行「税務易」設定的通行密碼則由8 - 10個英文字及數目字組成。英文字大小寫視為有別。",
+  "E-8036": "請輸入通行密碼。",
+  "E-8037": "税務編號格式由 8-16個英文及數目字組成。",
+  "E-8041": "税務易帳戶已被取消，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8042": "税務易帳戶已被取消，請點選 “申請税務易通行密碼”重新申請。",
+  "E-8043": "税務易帳戶己被撤銷，請點選 “申請税務易通行密碼”重新申請。",
+  "E-8047": "三次輸入不正確的密碼，操作終止。",
+  "E-8052": "系統未有紀錄，請輸入正確的税務編號。",
+  "E-8053": "未有紀錄，請於辦公時間聯絡熱線 (852)183 2011。",
+  "E-8057": "數碼證書的檔案大小應小於100KB。",
+  "E-8080":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8090": "使用本服務，你必須向税務局登記，並取得你的印花易帳戶號碼。",
+  "E-8091": "啟動密碼/通行密碼已被撤銷。",
+  "E-8092": "你在通行密碼項目內輸入了你的啟動密碼，請先啟動你的印花易帳戶。",
+  "E-8093": "輸入的密碼不正確。",
+  "E-8094": "輸入不正確，通行密碼已被撤銷。請向税務局重新登記方可使用本服務。",
+  "E-8095": "輸入的通行密碼不正確。",
+  "E-8096": "輸入不正確，通行密碼已被撤銷。請向税務局重新登記方可使用本服務。",
+  "E-8097":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8200":
+    "簽署發生錯誤，請重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8202":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8203":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8204":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8205":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8206":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8210":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8211":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8212":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8213":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8214":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8215":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8217":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8220":
+    "對不起，系統未能執行你的指令，請稍後重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8225":
+    "剛輸入的税務編號/通行密碼與簽署者不符，請於辦公時間聯絡熱線183 2011。",
+  "E-8226":
+    "剛輸入的税務編號/通行密碼與簽署者不符，請返回「個人資料」部份更正。",
+  "E-8227":
+    "剛輸入的税務編號/通行密碼與簽署者不符，請返回「個人資料」部份更正。",
+  "E-8228":
+    "剛輸入的税務編號/通行密碼與簽署者不符，請於辦公時間聯絡熱線183 2011。",
+  "E-8229": "「我的政府一站通」帳戶尚未與「税務易」帳戶連結。",
+  "E-8230": "系統未有記錄，請輸入正確的「我的政府一站通」帳戶。",
+  "E-8231": "輸入的「我的政府一站通」帳戶名稱不正確。",
+  "E-8232":
+    "簽署發生錯誤，請重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8233": "「我的政府一站通」帳戶已過期。",
+  "E-8234": "未能找到「我的政府一站通」帳戶。",
+  "E-8235": "「我的政府一站通」帳戶尚未啟動。",
+  "E-8236": "「我的政府一站通」帳戶已暫停使用。",
+  "E-8237": "「我的政府一站通」帳戶已鎖上。",
+  "E-8238": "「我的政府一站通」帳戶已被刪除。",
+  "E-8239": "「我的政府一站通」帳戶尚未與「税務易」帳戶連結。",
+  "E-8240": "密碼輸入不正確，請重新輸入。",
+  "E-8241":
+    "簽署發生錯誤，請重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8242":
+    "簽署發生錯誤，請重試。如問題持續發生，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8243": "輸入的「我的政府一站通」帳戶與配偶的相同。",
+  "E-8244": "請輸入「我的政府一站通」帳戶。",
+  "E-8245": "請輸入密碼。",
+  "E-8249": "數碼簽署的證書與帳戶登入時的數碼證書不相符",
+  "E-8261": "系統未有紀錄，請輸入正確的商業税務網站用戶名稱。",
+  "E-8262":
+    "商業税務網站用戶帳戶已被取消，請點選 “忘記用戶名稱/通行密碼”重新申請。",
+  "E-8263": "商業税務網站用戶帳戶已被暫停，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8264": "商業税務網站用戶帳戶已被取消，請於辦公時間聯絡熱線(852)183 2011。",
+  "E-8265":
+    "輸入不正確，請重新輸入通行密碼。商業税務網站設定的通行密碼則由10 - 16個英文字及數目字組成。英文字大小寫視為有別。",
+};
+
+// 875， 874
+const errorCodes = [
+  "E-8001",
+  "E-8002",
+  "E-8003", // 项目没有，这里没有
+  "E-8004",
+  "E-8005", // 项目没有，这里没有    "535-E-8005": "Invalid HKIC No. Please enter again.",  "535-E-8005": "輸入的香港身分證號碼格式不符，請再輸入。",
+  "E-8006", // 项目没有，这里没有    "547-E-8006": "Certificate Media Type must be selected.",  "547-E-8006": "必須選擇證書媒體類別。",
+  "E-8008",
+  "E-8020",
+  "E-8036",
+  "E-8037",
+  "E-8244",
+  "E-8245",
+  "E-3000", // 项目有，这里没有
+];
+//  "874-E-3000": "Please select one of the signing method.",
+//  "875-E-3000": "Please select one of the signing method.",
+// console.log(errorCodes);
+
+function changeErr(errorData = error, prefix = "875") {
+  let newData = {};
+  for (let key in errorData) {
+    if (errorData.hasOwnProperty(key) && errorCodes.includes(key)) {
+      const newKey = `${prefix}-${key}`;
+      newData[newKey] = errorData[key];
+    }
+  }
+  const json = JSON.stringify(newData);
+  return json;
+}
+
+const data1 = changeErr(error, "875");
+const data2 = changeErr(errorCn, "875");
+
+const data3 = changeErr(error, "874");
+const data4 = changeErr(errorCn, "874");
+
+const obj875 = {
+  en: data1,
+  cn: data2,
+};
+console.log("[ obj875  ] >", obj875);
+
+const obj874 = {
+  en: data3,
+  cn: data4,
+};
+
+console.log("[ obj874  ] >", obj874);
